839ff366b98d5d2caea0876c15797b643a6e1dd2
Austin <austin_pearce@ncsu.edu>
2022-03-01 09:34:43 -0500

    add plots to testing datasets

--- testing.R
+++ testing.R
@@ -2,11 +2,36 @@
 # package as an example for testing the lin_plateau, quad_plateau, mitscherlich,
 # and ALCC functions.
 
-cotton_k <- agridat::cate.potassium
-corn_n <- agridat::engelstad.nitro # QP
-yield_monitor <- agridat::gartner.corn
-yield_monitor <- agridat::lasrosas.corn
+library(tidyverse)
+theme_set(theme_classic())
+
+# relative cotton yield vs soil test potassium
+agridat::cate.potassium
+plot(agridat::cate.potassium$yield ~ agridat::cate.potassium$potassium)
+#
+agridat::engelstad.nitro # QP
+plot(agridat::engelstad.nitro$yield ~ agridat::engelstad.nitro$nitro)
+#
+agridat::gartner.corn
+
+agridat::gartner.corn %>% 
+    ggplot(aes(long, lat, color = mass)) +
+    geom_point(size = 7, alpha = 0.3) +
+    scale_color_viridis_c(option = "mako")
+#
+agridat::lasrosas.corn
+
+agridat::lasrosas.corn %>%
+    ggplot(aes(long, lat, color = yield)) +
+    geom_point(size = 3, alpha = 0.5) +
+    facet_wrap(vars(year)) +
+    scale_color_viridis_c(option = "mako")
+#
 agridat::sinclair.clover
+agridat::sinclair.clover %>% 
+    ggplot(aes(P, yield, color = yield)) +
+    geom_point(size = 2, alpha = 1) +
+    scale_color_viridis_c(option = "mako")
 
 
 cotton <- tibble(stk = agridat::cate.potassium$potassium,
