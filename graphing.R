b2d3ffc75bfb25de323b2f8e8ee1cab48af08574
Austin Pearce <awprc@pm.me>
2022-06-23 08:28:51 -0400

    Update graphing.R
    issue with robot condensed

--- graphing.R
+++ graphing.R
@@ -2,7 +2,8 @@
 sanserif <- c("Roboto Condensed")
 
 theme_gc <- 
-    theme_minimal(base_family = sanserif, base_size = 14) +
+    theme_minimal(#base_family = sanserif,
+        base_size = 14) +
     ggplot2::theme(
         plot.background = NULL,
         plot.margin = margin(t = 2, r = 2, b = 2, l = 2, unit = "pt"),
@@ -17,7 +18,7 @@ theme_gc <-
         axis.title.x = element_text(
             hjust = 0,
             margin = margin(t = 10, r = 0, b = 0, l = 0, unit = "pt")),
-        axis.text = element_text(family = sanserif),
+        #axis.text = element_text(family = sanserif),
         legend.title.align = 0,
         legend.key.height = unit(x = 5, units = "mm"),
         legend.justification = c(1, 1))
